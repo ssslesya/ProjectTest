c8b65c51bc529199777650e706fb3a52db67d1d6
ssslesya <128963774+ssslesya@users.noreply.github.com>
2023-11-27 02:55:27 +0400

    final commit

--- src/main/java/ru/olesya/ProjectTest/WebController.java
+++ src/main/java/ru/olesya/ProjectTest/WebController.java
@@ -3,6 +3,7 @@ package ru.olesya.ProjectTest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @CrossOrigin
@@ -12,16 +13,16 @@ public class WebController {
     @PostMapping("/calculateFrequency")
     public Map<String, Integer> calculateFrequency(@RequestBody String inputString) {
         Map<Character, Integer> frequencyMap = new HashMap<>();
-
         for (char c : inputString.toCharArray()) { // Подсчет частоты каждого символа во входной строке
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            if(Character.isLetter(c)){
+                frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            }
         }
 
-        Map<String, Integer> result = new HashMap<>();
+        Map<String, Integer> result = new LinkedHashMap<>();
         frequencyMap.entrySet().stream()// Сортировка по частоте
-                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-                .forEach(e -> result.put(String.valueOf(e.getKey()), e.getValue()));
-
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .forEach(e -> result.put(String.valueOf((e.getKey())), e.getValue()));
         return result;
     }
 
--- src/test/java/ru/olesya/ProjectTest/ProjectTestApplicationTests.java
+++ src/test/java/ru/olesya/ProjectTest/ProjectTestApplicationTests.java
@@ -6,35 +6,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
-import ru.olesya.ProjectTest.Model.InputData;
 
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.Map;
+import java.util.Objects;
 
-@WebMvcTest
 @AutoConfigureMockMvc
-@SpringBootTest
+@ContextConfiguration(classes= ProjectTestApplication.class)
+@WebMvcTest(WebController.class)
 class ProjectTestApplicationTests {
-		@Autowired
-		private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-		@Autowired
-		private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-		@Test
-		void testCalculateFrequency() throws Exception {
-			String inputString = "aaaaabcccc";
-			Map<String, Integer> expectedResult = Map.of("a", 5, "c", 4, "b", 1);
-
-			// Выполнение POST запроса на "/calculateFrequency" с входными данными, проверка результата
-			mockMvc.perform(post("/calculateFrequency")
-							.contentType(MediaType.APPLICATION_JSON)
-							.content(objectMapper.writeValueAsString(new InputData(inputString))))
-					.andExpect(status().isOk())
-					.andExpect(content().json(objectMapper.writeValueAsString(Map.of("frequency", expectedResult))));
+	@Test
+	void testCalculateFrequency() throws Exception {
+		String inputString = "aaaaabcccc";
+		String expectedResult = "{\"a\":5,\"c\":4,\"b\":1}";
+		// Выполнение POST запроса на "/calculateFrequency" с входными данными, проверка результата
+		String response = mockMvc.perform(post("/calculateFrequency")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(inputString)))
+				.andExpect(status().isOk())
+				.andReturn().getResponse().getContentAsString();
+		System.out.println(expectedResult.toString());
+		System.out.println("\n"+response);
+		if (!Objects.equals(expectedResult.toString(), response)) {
+			throw new Exception("Неправильный ответ");
 		}
+
 	}
+}
